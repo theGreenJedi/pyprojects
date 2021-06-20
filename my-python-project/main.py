cac022af327029ac35986338aa8248b02a9ee141
theGreenJedi <PeteIntheCloud@pm.me>
2021-06-20 13:30:53 -0700

    py scratch

---
+++ my-python-project/main.py
@@ -0,0 +1,9 @@
+calculation_to_units=24
+name_of_unit="hours"
+
+
+def day_to_units(num_of_days)
+    print(f"20 dyas are {20*calculation_to_units} {name_of_unit}")
+    print("All good!")
+
+days_to_units(35)
---
+++ pythonProject1/main.py
@@ -0,0 +1,3 @@
+
+calc_to seconds*0*60
+
